295e7f0b7c7d708a96974f1518028fae5ca3de1d
tercette <leandro.tercette@gmail.com>
2022-10-10 22:36:05 -0300

    webapp update

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-apps',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apps.component.css']
 })
 export class AppsComponent implements OnInit {
+  Sendmessage: boolean = true;
+  visible: boolean = false;
+  alert: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  onclick() {
+    this.Sendmessage = !this.Sendmessage;
+    this.visible = !this.visible;
+  }
+
+  backToHome() {
+    this.router.navigate([''])
+  }
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+        const email = contactForm.value;
+        const headers = new HttpHeaders({ 'Content-type': 'application/json' });
+        this.http
+            .post(
+                'https://formspree.io/f/meqvpjwz',
+                {
+                    replyto: email.email,
+                    subject: email.subject,
+                    messages: email.messages,
+                },
+
+                { headers: headers }
+            )
+            .subscribe((response) => {
+                console.log(response);
+            });
+
+        this.alert = true;
+        if (contactForm.valid) {
+            console.log('Formulario enviado.');
+            contactForm.reset();
+        }
+    }
+}
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,64 +1,65 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {
-  Component,
-  Injectable,
-  OnInit,
-  ViewEncapsulation,
+    Component,
+    Injectable,
+    OnInit,
+    ViewEncapsulation,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Injectable()
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  Sendmessage: boolean = true;
-  visible: boolean = false;
-  alert: boolean = false;
+    Sendmessage: boolean = true;
+    visible: boolean = false;
+    alert: boolean = false;
 
-  constructor(private http: HttpClient, private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
-  ngOnInit(): void { }
+    ngOnInit(): void { }
 
-  onclick() {
-    this.Sendmessage = !this.Sendmessage;
-    this.visible = !this.visible;
-  }
-
-  app(){
-    this.router.navigate(['/apps'])
-  }
-
-  onSubmit(contactForm: NgForm) {
-    if (contactForm.valid) {
-      const email = contactForm.value;
-      const headers = new HttpHeaders({ 'Content-type': 'application/json' });
-      this.http
-        .post(
-          'https://formspree.io/f/meqvpjwz',
-          {
-            replyto: email.email,
-            subject: email.subject,
-            messages: email.messages,
-          },
-
-          { headers: headers }
-        )
-        .subscribe((response) => {
-          console.log(response);
-        });
-
-      this.alert = true;
-      if (contactForm.valid) {
-        console.log('Formulario enviado.');
-        contactForm.reset();
-      }
+    onclick() {
+        this.Sendmessage = !this.Sendmessage;
+        this.visible = !this.visible;
     }
-  }
+
+    app() {
+        this.router.navigate(['/apps'])
+    }
+
+
+    onSubmit(contactForm: NgForm) {
+        if (contactForm.valid) {
+            const email = contactForm.value;
+            const headers = new HttpHeaders({ 'Content-type': 'application/json' });
+            this.http
+                .post(
+                    'https://formspree.io/f/meqvpjwz',
+                    {
+                        replyto: email.email,
+                        subject: email.subject,
+                        messages: email.messages,
+                    },
+
+                    { headers: headers }
+                )
+                .subscribe((response) => {
+                    console.log(response);
+                });
+
+            this.alert = true;
+            if (contactForm.valid) {
+                console.log('Formulario enviado.');
+                contactForm.reset();
+            }
+        }
+    }
 
 
 
